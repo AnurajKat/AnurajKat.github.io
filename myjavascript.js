aa34ae0ecff61cb877b78f10d214d0790062e66c
AnurajK <anuraj141kathait@outlook.com>
2016-10-09 23:47:26 +0530

    navbar
    linked index to the welcomepage, added navigation bar to the second
    page. some javascript

--- myjavascript.js
+++ myjavascript.js
@@ -3,3 +3,11 @@ function alertuser() {
     "use strict";
     window.alert("abhi kaam nhi karega pagle!!");
 }
+function openNav() {
+    "use strict";
+    document.getElementById("mysidenav").style.width = "225px";
+}
+function closeNav() {
+    "use strict";
+    document.getElementById("mysidenav").style.width = "0";
+}
