f0b0f4783f9586526f86e495385d7cd2ac16d30d
AnurajK <anuraj141kathait@outlook.com>
2016-10-10 23:40:43 +0530

    navbar, frames
    fixed the position of navigation bar. Home redirects to the quote page
    also there is a logout button which does not exactly looks like a logout
    button.

--- myjavascript.js
+++ myjavascript.js
@@ -3,11 +3,29 @@ function alertuser() {
     "use strict";
     window.alert("abhi kaam nhi karega pagle!!");
 }
-function openNav() {
-    "use strict";
-    document.getElementById("mysidenav").style.width = "225px";
-}
 function closeNav() {
     "use strict";
+    document.getElementById("menubtn").style.marginLeft = "0px";
     document.getElementById("mysidenav").style.width = "0";
+    
+}
+function openNav() {
+    "use strict";
+    document.getElementById("menubtn").style.marginLeft = "225px";
+    document.getElementById("mysidenav").style.width = "225px";
+   
+}
+
+function clickCheck() {
+    "use strict";
+    var x = document.getElementById("menubtn");
+    if (x.checked === 1) {
+        openNav();
+    } else {
+        closeNav();
+    }
+}
+function gotoLogin() {
+    "use strict";
+    window.location.href = 'index.html';
 }
