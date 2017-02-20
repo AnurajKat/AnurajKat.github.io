4b0a27117f80f67ca105f67c47536a58821294a6
AnurajK <anuraj141kathait@outlook.com>
2017-02-21 02:03:13 +0530

    added draggable menu
    spent almost a week on making a draggable menu.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
-$(".menu").mousedown(funtion(){
-                     $(this).toggleClass("closed");
-
-if($(this).hasClass("closed")){
-    $(".main.button").text("Menu");
-} else {
-    $(".menu.button").text("Close");
+/*
+var picture_location = "";
+function newpicpage(picname, picheading) {
+    "use strict";
+    var location = picname;
+    picture_location = location;
+    window.alert(picture_location);
+    var newurl = "picpage.html?" + "location=" + picture_location +  "&heading=" + picheading; 
+    window.open(newurl);
 }
-                     });
+
+function validateEmail(email) {
+    "use strict";
+    var re =  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return re.test(email);
+}*/
+ $(function() {
+    $(".mainmenu").draggable();});
+        
+        var b=true;
+        function moggle() {
+            if(b) {
+                document.getElementById("fn").className = "menu";
+                document.getElementById("btnmenu").innerHTML="Close";
+                b = false;
+            } else {
+                b = true;
+                document.getElementById("fn").className += " closed";
+                document.getElementById("btnmenu").innerHTML= "Menu";
+            }
+        }
+         
+
+function validateEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
